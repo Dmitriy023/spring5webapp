e0d19e00976012015cae6757b01fffbb1ae2ef11
Dmitriy <dmitriy091991@gmail.com>
2018-10-14 00:08:33 +0400

    Adding templates

--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -13,7 +13,11 @@ public class Book {
 
     private String title;
     private String isbn;
-    private String publisher;
+
+    @OneToOne
+    private Publisher publisher;
+
+
 
     @ManyToMany
     @JoinTable(name = "author_book",
@@ -24,13 +28,13 @@ public class Book {
     public Book() {
     }
 
-    public Book(String title, String isbn, String publisher) {
+    public Book(String title, String isbn, Publisher publisher) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
     }
 
-    public Book(String title, String isbn, String publisher, Set<Author> authors) {
+    public Book(String title, String isbn, Publisher publisher, Set<Author> authors) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
@@ -53,11 +57,11 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public String getPublisher() {
+    public Publisher getPublisher() {
         return publisher;
     }
 
-    public void setPublisher(String publisher) {
+    public void setPublisher(Publisher publisher) {
         this.publisher = publisher;
     }
 
@@ -67,5 +71,12 @@ public class Book {
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
--- src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
@@ -3,6 +3,6 @@ package guru.springframework.spring5webapp.repositories;
 import guru.springframework.spring5webapp.model.Book;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BookInterface  extends CrudRepository<Book,Long>{
+public interface BookRepository extends CrudRepository<Book,Long>{
 
 }
